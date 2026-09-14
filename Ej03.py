3d6c5fa876e40eefa1ef0b9263d707611ed8028f
Samuel Fernández Rey <a26samuelfr@iesantonlosada.gal>
2026-09-14 14:10:57 +0200

    Ejercicio 3 hecho

---
+++ Ej03.py
@@ -0,0 +1,5 @@
+nombre = input("Escribe tu nombre: ")
+apellido1 = input("Escribe tu primer apellido: ")
+apellido2 = input("Escribe tu segundo apellido: ")
+
+print("El usuario", apellido1, apellido2, nombre, "se ha registrado correctamente")
