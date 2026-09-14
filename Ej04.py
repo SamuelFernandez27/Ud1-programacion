2b444295d232a892e5115ae092549e6e1bc81503
Samuel Fernández Rey <a26samuelfr@iesantonlosada.gal>
2026-09-14 14:21:59 +0200

    Ejercicio 4 hecho

---
+++ Ej04.py
@@ -0,0 +1,9 @@
+base = int(input("Dime la base del triángulo: "))
+altura = int(input("Dime la altura del triángulo: "))
+
+print("El área del triángulo es:", base * altura / 2)
+
+radio = int(input("Dime el radio de la circunferencia: "))
+pi = int(3.1416)
+
+print("Su perímetro es:", 2 * pi * radio, "y su área es:", pi + radio**2)
